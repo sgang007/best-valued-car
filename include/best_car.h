c49468f2047919b0e1419cf2e938c6551fe84f79
Shubhojyoti <gangulyshubhojyoti@gmail.com>
2015-04-22 16:19:48 +0530

    Update techniques

--- include/best_car.h
+++ include/best_car.h
@@ -4,16 +4,28 @@
 #include <iostream>
 #include <decision_tree.h>
 #include <boost>
-#include <template>
+#include <string>
+#include <vector>
 #include <algorithm>
 
 class best_car
 {
-public:
-	best_car(tree_file,parameter_file);
-	~best_car();
 private:
-	void init();
+	std::vector<std::vector<int>> mat_engineer; 
+	std::vector<std::vector<int>> mat_comsumer; 
+	std::vector<std::vector<int>> mat_total; 
+	tree_search tree;
+	void init();//generate mat_engineer
+	void consumer_matrix(int);//calculate mat_consumer
+	void evaluate_costs();//total cost evaluated
+	void show_best_car(int);//rank of car based on match passed as parameter
+	//searches tree based on total cost of each attribute passed in an array
+	void search(std::vector<int>);
+
+public:
+	best_car(std::string,std::string);
+	~best_car();
+
 	
 };
 
